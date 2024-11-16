fc54db179dfbf25af73e6a3661db11ab575e6657
reginaldouninter <161330982+reginaldouninter@users.noreply.github.com>
2024-11-16 11:26:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ pygame.init()
 window = pygame.display.set_mode(size=(600, 480))
 print('Setup End')
 
-print('Setup Start')
+print('Loop Start')
 while True:
     # Check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print('Quitting...')
             pygame.quit()  # Close Window
             quit()  # end pygame
