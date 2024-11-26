53370d3a9189373577b520f23381329e2fb3eb0c
Naldo <reginaldosm@gmail.com>
2024-11-26 12:06:19 -0300

    Move Payer1

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from code.Background import Background
-from code.Const import WIN_WIDTH
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Player import Player
 
 
 class EntityFactory:
@@ -15,3 +16,5 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (0,0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH,0)))
                 return list_bg
+            case 'Player1':
+                return Player('Player1', (10, WIN_HEIGHT / 2))
--- code/Level.py
+++ code/Level.py
@@ -19,6 +19,7 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.entity_list.append(EntityFactory.get_entity('Player1'))
         self.timeout = 20000 # 20 segundos
 
 
