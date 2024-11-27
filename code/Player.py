e64447f01d7779828723daf660b0828c89e6eabc
Naldo <reginaldosm@gmail.com>
2024-11-27 11:57:28 -0300

    Creating Players and Enemies Moving players and Enemies

--- code/Player.py
+++ code/Player.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 import pygame.key
 
-from code.Const import ENTITY_SPEED, WIN_HEIGHT, WIN_WIDTH
+from code.Const import ENTITY_SPEED, WIN_HEIGHT, WIN_WIDTH, PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, \
+    PLAYER_KEY_RIGHT
 from code.Entity import Entity
 
 
@@ -12,12 +13,12 @@ class Player(Entity):
 
     def move(self, ):
         pressed_key = pygame.key.get_pressed()
-        if pressed_key[pygame.K_UP] and self.rect.top > 0:
+        if pressed_key[PLAYER_KEY_UP[self.name]] and self.rect.top > 0:
             self.rect.centery -= ENTITY_SPEED[self.name]
-        if pressed_key[pygame.K_DOWN] and self.rect.bottom < WIN_HEIGHT:
+        if pressed_key[PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < WIN_HEIGHT:
             self.rect.centery += ENTITY_SPEED[self.name]
-        if pressed_key[pygame.K_LEFT] and self.rect.left > 0:
+        if pressed_key[PLAYER_KEY_LEFT[self.name]] and self.rect.left > 0:
             self.rect.centerx -= ENTITY_SPEED[self.name]
-        if pressed_key[pygame.K_RIGHT] and self.rect.right < WIN_WIDTH:
+        if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH:
             self.rect.centerx += ENTITY_SPEED[self.name]
         pass
